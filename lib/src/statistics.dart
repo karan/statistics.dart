b51ca2658e25e52d4371a77e894e98904c2541eb
Karan Goel <karan@goel.im>
2014-11-30 19:42:34 -0800

    add implementation for mean

--- lib/src/statistics.dart
+++ lib/src/statistics.dart
@@ -1,1 +1,27 @@
 part of startistics;
+
+
+/// Return a of the given numeric data.
+/// If [start] is given, it is added to the sum. If [data] is empty,
+/// 0 is returned.
+num _sum(List data, [num start=0]) {
+  num sum = start;
+  for (num x in data) {
+    sum += x;
+  }
+  return sum;
+}
+
+
+/// Return the sample arithmetic mean of [data].
+/// If [data] is empty, [StatisticsException] will be thrown.
+num mean(List data) {
+  if (data.runtimeType == Iterable) {
+    data = data.toList();
+  }
+  int n = data.length;
+  if (n < 1) {
+    throw new StatisticsException('mean requires at least one data point.');
+  }
+  return _sum(data) / n;
+}
--- lib/statistics.dart
+++ lib/statistics.dart
@@ -1,3 +1,4 @@
 library statistics;
 
 part 'src/statistics.dart';
+part 'src/statistics_exception.dart';
